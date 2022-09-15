d16723c4ffa9b23e066724f8586c78c38ca5bb11
Ben Aepli <baepli@jhcschool.org>
2022-09-15 16:33:16 -0600

    added controlled mode, started opencv detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorflowObjectDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TensorflowObjectDetector.java
@@ -10,14 +10,14 @@ import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
 import java.util.List;
 
-public class ObjectDetection {
+public class TensorflowObjectDetection {
 
     private static final String VUFORIA_KEY = "AYz3cgf/////AAABma+WDnhtL033gWAJtplg3fpuI61DBp5XPjU0nVdmgTGfW6TDs4O1njj5m161MEBsneA41OCmCa9APvr1nya5XVzJ9OaTNeKd/xHuYEDGgW8G0+IXhwhJ0DaTWHgnDv0UfMh/qTvIvpvoqXM2OKmlzZA+P1FF/vO75lqUcyEsRdh3L7/qtcJEKplAIPldr1uhGku9ZEdckjIFfl/Dhf/G1plF3R6RVOlW8oUHf4JpJTPMw02MdvVxhGnFyHQ4lF6lpZ0pZNJKJY1vQ35gn8WnH9mydbAtfZRSv+jSSmv+rgho7pacbktP9P6chMcMA8vZJWA1vBdXHtfTZI6IFEEV4xpaA4A4L8s7WQvGMgTYlFiZ";
 
     private VuforiaLocalizer vuforia;
     private TFObjectDetector tfod;
 
-    public ObjectDetection(int viewId, CameraName cameraName, String modelFile, String[] labels) {
+    public TensorflowObjectDetection(int viewId, CameraName cameraName, String modelFile, String[] labels) {
         initVuforia(cameraName);
         initTfod(viewId, modelFile, labels);
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opencv/OpenObjectDetector.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.CameraName;
+import org.openftc.easyopencv.OpenCvCamera;
+
+public class OpenObjectDetector {
+    private OpenCvCamera camera;
+
+    private int cameraWidth;
+    private int cameraHeight;
+
+    public OpenObjectDetector(int viewId, CameraName cameraName, ) {
+    }
+
+}
